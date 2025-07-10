61d392370f828254ad57f269ee73587889055461
AxlRosales <axelerl2000@gmail.com>
2025-07-10 17:55:53 -0600

    Primer Commit

---
+++ Graphics2DExamen/src/main/java/com/mycompany/graphics2dexamen/Graphics2DExamen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.graphics2dexamen;
+
+/**
+ *
+ * @author Axel Rosales
+ */
+public class Graphics2DExamen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
